9b3f011d21174cc36ec6fe66195befaa25dab5b1
jgabriellacerda <gabriel.lacerda@engenharia.ufjf.br>
2022-03-15 23:02:03 -0300

    Retirada chave extra

--- assets/js/home.bundle.js
+++ assets/js/home.bundle.js
@@ -1,50 +1,82 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const ListComponent = require('./ListComponent');
+const ListComponent = require("./ListComponent");
 
-require('./ListComponent');
+require("./ListComponent");
 
-window.ListComponent = ListComponent;
-},{"./ListComponent":2}],2:[function(require,module,exports){
-let ListComponent = function(name) {
+const HomeComponent = () => {
+	let listCounter = 0;
+	let lists = [];
 
-    let todoCounter = 0;
+	function addList() {
+		console.log("add list");
+		listCounter++;
+		let listID = listCounter;
+		let newList = new ListComponent("Nova lista", listID);
 
-    function pushTodo(todo) {
-        this.todos.push(todo);
-    }
+		this.lists.push(newList);
+		console.log(newList);
+	}
 
-    function popTodo(id) {
-        let filteredTodos = this.todos.filter(todo => {
-            return todo.ID !== id;
-        });
-        this.todos = filteredTodos;
-    }
+	function deleteList(ID) {
+		let filteredLists = this.lists.filter((list) => {
+			return list.ID !== ID;
+		});
+		this.lists = filteredLists;
+	}
 
-    function addTodo(todoText) {
-        if (todoText != "") {
-            todoCounter++;
-            let newTodo = {
-                ID: todoCounter,
-                text: todoText
-            }
-            this.pushTodo(newTodo);
-        }
-    }
-
-    function deleteTodo(id) {
-        this.popTodo(id);
-    }
-
-    return {
-        name,
-        todos: [],
-        pushTodo,
-        popTodo,
-        addTodo,
-        deleteTodo,
-    }
+	return {
+		lists,
+		addList,
+		deleteList,
+	};
 };
 
+window.ListComponent = ListComponent;
+window.HomeComponent = HomeComponent;
+
+},{"./ListComponent":2}],2:[function(require,module,exports){
+let ListComponent = function (name, ID) {
+	let todoCounter = 0;
+	let todos = [];
+
+	function pushTodo(todo) {
+		this.todos.push(todo);
+	}
+
+	function popTodo(ID) {
+		let filteredTodos = this.todos.filter((todo) => {
+			return todo.ID !== ID;
+		});
+		this.todos = filteredTodos;
+	}
+
+	function addTodo(todoText) {
+		if (todoText != "") {
+			todoCounter++;
+			let todoID = todoCounter;
+			let newTodo = {
+				ID: todoID,
+				text: todoText,
+			};
+			this.pushTodo(newTodo);
+		}
+	}
+
+	function deleteTodo(ID) {
+		this.popTodo(ID);
+	}
+
+	return {
+		ID,
+		name,
+		todos,
+		pushTodo,
+		popTodo,
+		addTodo,
+		deleteTodo,
+	};
+};
 
 module.exports = ListComponent;
+
 },{}]},{},[1]);
