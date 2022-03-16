046bbba28cda1ea3adff8f86720ade22b3b027e6
jgabriellacerda <gabriel.lacerda@engenharia.ufjf.br>
2022-03-15 22:50:43 -0300

    Edição do nome da lista

--- assets/js/home.bundle.js
+++ assets/js/home.bundle.js
@@ -5,46 +5,58 @@ require('./ListComponent');
 
 window.ListComponent = ListComponent;
 },{"./ListComponent":2}],2:[function(require,module,exports){
-let ListComponent = function(name) {
+let ListComponent = function (name) {
+	let todoCounter = 0;
+	let todos = [];
 
-    let todoCounter = 0;
+	function pushTodo(todo) {
+		this.todos.push(todo);
+	}
 
-    function pushTodo(todo) {
-        this.todos.push(todo);
-    }
+	function popTodo(id) {
+		let filteredTodos = this.todos.filter((todo) => {
+			return todo.ID !== id;
+		});
+		this.todos = filteredTodos;
+	}
 
-    function popTodo(id) {
-        let filteredTodos = this.todos.filter(todo => {
-            return todo.ID !== id;
-        });
-        this.todos = filteredTodos;
-    }
+	function addTodo(todoText) {
+		if (todoText != "") {
+			todoCounter++;
+			let newTodo = {
+				ID: todoCounter,
+				text: todoText,
+			};
+			this.pushTodo(newTodo);
+		}
+	}
 
-    function addTodo(todoText) {
-        if (todoText != "") {
-            todoCounter++;
-            let newTodo = {
-                ID: todoCounter,
-                text: todoText
-            }
-            this.pushTodo(newTodo);
-        }
-    }
+	function deleteTodo(id) {
+		this.popTodo(id);
+	}
 
-    function deleteTodo(id) {
-        this.popTodo(id);
-    }
+	function updateName(newName) {
+		let oldName = this.name;
+		if (newName != "") {
+			this.name = newName;
+		} else {
+			// Reset name
+			this.name = "";
+			this.name = oldName;
+		}
+	}
 
-    return {
-        name,
-        todos: [],
-        pushTodo,
-        popTodo,
-        addTodo,
-        deleteTodo,
-    }
+	return {
+		name,
+		todos,
+		pushTodo,
+		popTodo,
+		addTodo,
+		deleteTodo,
+		updateName,
+	};
 };
 
-
 module.exports = ListComponent;
+
 },{}]},{},[1]);
--- src/js/ListComponent.js
+++ src/js/ListComponent.js
@@ -1,43 +1,53 @@
-let ListComponent = function(name) {
+let ListComponent = function (name) {
+	let todoCounter = 0;
+	let todos = [];
 
-    let todoCounter = 0;
-    let todos = [];
+	function pushTodo(todo) {
+		this.todos.push(todo);
+	}
 
-    function pushTodo(todo) {
-        this.todos.push(todo);
-    }
+	function popTodo(id) {
+		let filteredTodos = this.todos.filter((todo) => {
+			return todo.ID !== id;
+		});
+		this.todos = filteredTodos;
+	}
 
-    function popTodo(id) {
-        let filteredTodos = this.todos.filter(todo => {
-            return todo.ID !== id;
-        });
-        this.todos = filteredTodos;
-    }
+	function addTodo(todoText) {
+		if (todoText != "") {
+			todoCounter++;
+			let newTodo = {
+				ID: todoCounter,
+				text: todoText,
+			};
+			this.pushTodo(newTodo);
+		}
+	}
 
-    function addTodo(todoText) {
-        if (todoText != "") {
-            todoCounter++;
-            let newTodo = {
-                ID: todoCounter,
-                text: todoText
-            }
-            this.pushTodo(newTodo);
-        }
-    }
+	function deleteTodo(id) {
+		this.popTodo(id);
+	}
 
-    function deleteTodo(id) {
-        this.popTodo(id);
-    }
+	function updateName(newName) {
+		let oldName = this.name;
+		if (newName != "") {
+			this.name = newName;
+		} else {
+			// Reset name
+			this.name = "";
+			this.name = oldName;
+		}
+	}
 
-    return {
-        name,
-        todos,
-        pushTodo,
-        popTodo,
-        addTodo,
-        deleteTodo,
-    }
+	return {
+		name,
+		todos,
+		pushTodo,
+		popTodo,
+		addTodo,
+		deleteTodo,
+		updateName,
+	};
 };
-
 
 module.exports = ListComponent;
