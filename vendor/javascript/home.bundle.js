9aae28483ddde3f531f74d3108a46703c01556c7
jgabriellacerda <gabriel.lacerda@engenharia.ufjf.br>
2022-03-17 01:34:18 -0300

    Implementadas consultas para buscar dados

--- vendor/javascript/home.bundle.js
+++ vendor/javascript/home.bundle.js
@@ -3,16 +3,51 @@ const ListComponent = require("./ListComponent");
 
 require("./ListComponent");
 
-const HomeComponent = () => {
-	let listCounter = 0;
+const HomeComponent = (token) => {
 	let lists = [];
 
-	function addList() {
-		listCounter++;
-		let listID = listCounter;
-		let newList = new ListComponent("Nova lista", listID);
+	let url = `http://${window.location.host}`;
 
+	async function init() {
+		await this.getLists();
+	}
+
+	async function getLists() {
+		let response = await fetch(`${url}/lists.json`, {
+			method: "GET",
+		});
+		console.log(response);
+		data = await response.json();
+		console.log(data);
+		for (let list of data) {
+			console.log(list);
+			let newList = new ListComponent(list.name, list.id, token);
+			this.pushList(newList);
+		}
+	}
+
+	function pushList(newList) {
 		this.lists.push(newList);
+	}
+
+	async function addList() {
+		let formData = new FormData();
+		formData.append("list[name]", "Nova lista");
+		formData.append("authenticity_token", this.token);
+
+		let response = await fetch(`${url}/lists.json`, {
+			method: "POST",
+			body: formData,
+			headers: {
+				"X-Requested-With": "XMLHttpRequest",
+			},
+		});
+
+		if (response.status == 201) {
+			let list = await response.json();
+			let newList = new ListComponent(list.name, list.id, token);
+			this.pushList(newList);
+		}
 	}
 
 	async function confirmDelete(id) {
@@ -35,18 +70,39 @@ const HomeComponent = () => {
 		}
 	}
 
-	function deleteList(ID) {
+	async function deleteList(id) {
+		let formData = new FormData();
+		formData.append("authenticity_token", this.token);
+		let response = await fetch(`${url}/lists/${id}.json`, {
+			method: "DELETE",
+			body: formData,
+			headers: {
+				"X-Requested-With": "XMLHttpRequest",
+			},
+		});
+
+		if (response.status == 204) {
+			this.popList(id);
+		}
+	}
+
+	function popList(id) {
 		let filteredLists = this.lists.filter((list) => {
-			return list.ID !== ID;
+			return list.id !== id;
 		});
 		this.lists = filteredLists;
 	}
 
 	return {
+		init,
 		lists,
 		addList,
 		deleteList,
 		confirmDelete,
+		pushList,
+		popList,
+		getLists,
+		token,
 	};
 };
 
@@ -56,46 +112,132 @@ window.ListComponent = ListComponent;
 window.HomeComponent = HomeComponent;
 
 },{"./ListComponent":2}],2:[function(require,module,exports){
-let ListComponent = function (name, ID) {
-	let todoCounter = 0;
+let ListComponent = function (name, id, token) {
 	let todos = [];
+
+	let url = `http://${window.location.host}`;
+
+	async function init() {
+		await this.getTodos();
+	}
+
+	async function getTodos() {
+		let response = await fetch(`${url}/todos.json`, {
+			method: "GET",
+		});
+
+		todos = await response.json();
+
+		let listTodos = todos.filter((todo) => {
+			return todo.list_id === this.id;
+		});
+		listTodos.forEach((todo) => {
+			this.pushTodo(todo);
+		});
+	}
 
 	function pushTodo(todo) {
 		this.todos.push(todo);
 	}
 
-	function popTodo(ID) {
+	function popTodo(id) {
 		let filteredTodos = this.todos.filter((todo) => {
-			return todo.ID !== ID;
+			return todo.id !== id;
 		});
 		this.todos = filteredTodos;
 	}
 
-	function addTodo(todoText) {
-		if (todoText != "") {
-			todoCounter++;
-			let todoID = todoCounter;
-			let newTodo = {
-				ID: todoID,
-				text: todoText,
-			};
-			this.pushTodo(newTodo);
+	async function addTodo(todoTask) {
+		if (checkTodoTask(todoTask)) {
+			let formData = new FormData();
+			formData.append("todo[list_id]", this.id);
+			formData.append("todo[task]", todoTask);
+			formData.append("authenticity_token", this.token);
+
+			let response = await fetch(`${url}/todos.json`, {
+				method: "POST",
+				body: formData,
+				headers: {
+					"X-Requested-With": "XMLHttpRequest",
+				},
+			});
+
+			if (response.status == 201) {
+				let todo = await response.json();
+				this.pushTodo(todo);
+			} else {
+				showErrorAlert();
+			}
 		}
 	}
 
-	function deleteTodo(id) {
-		this.popTodo(id);
+	async function deleteTodo(id) {
+		let formData = new FormData();
+		formData.append("authenticity_token", this.token);
+		let response = await fetch(`${url}/todos/${id}.json`, {
+			method: "DELETE",
+			body: formData,
+			headers: {
+				"X-Requested-With": "XMLHttpRequest",
+			},
+		});
+
+		if (response.status == 204) {
+			this.popTodo(id);
+		} else {
+			showErrorAlert();
+		}
 	}
 
-	function updateName(newName) {
+	async function updateName(newName) {
 		let oldName = this.name;
-		if (newName != "") {
-			this.name = newName;
+		if (checkName(newName)) {
+			let formData = new FormData();
+			formData.append("list[name]", newName);
+			formData.append("authenticity_token", this.token);
+
+			let response = await fetch(`${url}/lists/${this.id}.json`, {
+				method: "PATCH",
+				body: formData,
+				headers: {
+					"X-Requested-With": "XMLHttpRequest",
+				},
+			});
+
+			if (response.status == 200) {
+				let list = await response.json();
+				this.setName(list.name);
+			} else {
+				showErrorAlert();
+				// Reset name
+				this.name = "";
+				this.name = oldName;
+			}
 		} else {
 			// Reset name
 			this.name = "";
 			this.name = oldName;
 		}
+	}
+
+	function setName(name) {
+		if (checkName(name)) {
+			this.name = name;
+		}
+	}
+
+	function checkName(name) {
+		if (name == "") {
+			return false;
+		}
+		return true;
+	}
+
+	function checkTodoTask(task) {
+		if (task == "") {
+			return false;
+		}
+		return true;
 	}
 
 	async function randomTodo() {
@@ -122,15 +264,21 @@ let ListComponent = function (name, ID) {
 	}
 
 	return {
-		ID,
+		init,
+		id,
 		name,
 		todos,
 		pushTodo,
 		popTodo,
 		addTodo,
+		checkTodoTask,
 		deleteTodo,
 		updateName,
+		checkName,
+		setName,
 		randomTodo,
+		getTodos,
+		token,
 	};
 };
 
